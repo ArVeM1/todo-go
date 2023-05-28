19ec69627c87b86c3b0709dccbdc2adecbce0a87
Amir <dusenbaev101@gmail.com>
2023-05-28 13:04:57 +0400

    init

---
+++ cmd/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"github.com/ArVeM1/todo-go"
+	"github.com/ArVeM1/todo-go/pkg/handler"
+	"github.com/ArVeM1/todo-go/pkg/repository"
+	"github.com/ArVeM1/todo-go/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
+	"log"
+	"os"
+)
+
+func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env vatiables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	srv := new(todo.Server)
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}
---
+++ pkg/handler/list.go
@@ -0,0 +1,25 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func (h *Handler) createList(c *gin.Context) {
+
+}
+
+func (h *Handler) getAllList(c *gin.Context) {
+
+}
+
+func (h *Handler) getListById(c *gin.Context) {
+	//id := c.Param("id")
+}
+
+func (h *Handler) updateList(c *gin.Context) {
+
+}
+
+func (h *Handler) deleteList(c *gin.Context) {
+
+}
